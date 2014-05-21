a18da35df2e1ca739698800f17fd2440a4fcd716
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2014-05-21 15:58:03 +0200

    Improved override

--- app/code/community/Hackathon/MultiServer/Model/Index/Process.php
+++ app/code/community/Hackathon/MultiServer/Model/Index/Process.php
@@ -17,13 +17,9 @@ class Hackathon_MultiServer_Model_Index_Process extends Mage_Index_Model_Process
         if ($this->_lockFile === null) {
             $varDir = Mage::getConfig()->getVarDir('locks');
             $file = $varDir . DS . 'index_process_'.$this->getId().'.lock';
-            if (is_file($file)) {
-                $this->_lockFile = fopen($file, 'w');
-            } else {
-                $this->_lockFile = fopen($file, 'x');
-            }
-            fwrite($this->_lockFile, date('r'));
-            Mage::dispatchEvent('multiserver_file_change',array('file_path' => $file, 'change_time' => time()));
+            parent::_getLockFile();
+            Mage::dispatchEvent( 'multiserver_file_change', array( 'file_path' => $file,
+                                                                   'change_time' => microtime(true) ) );
         }
         return $this->_lockFile;
     }
