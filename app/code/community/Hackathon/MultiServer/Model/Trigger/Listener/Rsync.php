5d4fbd9de66c2f428062f5a3a65340a5fbca1e41
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2014-05-21 13:31:43 +0200

    Rsync proof-of-concept event is working

--- app/code/community/Hackathon/MultiServer/Model/Trigger/Listener/Rsync.php
+++ app/code/community/Hackathon/MultiServer/Model/Trigger/Listener/Rsync.php
@@ -29,7 +29,7 @@ class Hackathon_MultiServer_Model_Trigger_Listener_Rsync
     function trigger( $observer ) {
         $filePath   = $observer->getFilePath();    // Required, full server path
         $changeTime = $observer->getChangeTime();  // optional
-        $action     = $observer->getAction();      // optional, defaults to 'update'
+        $action     = $observer->getAction();      // optional, create/update/delete, defaults to 'update'
 
         if ( empty($changeTime) ) {
             $changeTime = microtime( true );
@@ -37,13 +37,6 @@ class Hackathon_MultiServer_Model_Trigger_Listener_Rsync
         if ( empty($action) ) {
             $action = 'update';
         }
-        /**
-         * Action can be:
-         *
-         * - create
-         * - update
-         * - delete
-         */
 
         $localRoot = $this->serverList['local']['mage_root'];
         if ( 0 === strpos( $filePath, $localRoot ) ) {
@@ -51,14 +44,24 @@ class Hackathon_MultiServer_Model_Trigger_Listener_Rsync
             $relativeFile = substr( $filePath, strlen($localRoot) ); // starts with a slash
             foreach ( $this->serverList as $key => $serverInfo ) {
                 if ( 'local' != $key ) {
+                    $sshTarget = $serverInfo['user'].'@'.$serverInfo['host'];
                     $targetFile = $serverInfo['mage_root'] . $relativeFile;
-                    $destination = $serverInfo['user'].'@'.$serverInfo['host'].':'.$targetFile;
-                    $rsyncCmd = sprintf( 'rsync -a -z -F --rsh="/usr/bin/ssh -p%d" %s %s',
+                    $remoteDir = dirname( $targetFile );
+
+                    // Make sure the dir exists via SSH.
+                    $mkdirCmd = sprintf( 'ssh -p%d %s mkdir -p %s',
+                                         $serverInfo['port'],
+                                         $sshTarget,
+                                         $remoteDir );
+                    shell_exec( $mkdirCmd );
+
+                    // Rsync the file
+                    $destination = $sshTarget.':'.$targetFile;
+                    $rsyncCmd = sprintf( 'rsync -azp --rsh="/usr/bin/ssh -p%d" %s %s',
                                          $serverInfo['port'],
                                          $filePath,
                                          $destination );
                     shell_exec( $rsyncCmd );
-                    // @TODO Fix rsync error.
                 }
             }
         }
